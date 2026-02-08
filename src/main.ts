5feaacedd9e1d99ee3f366eed056a1fb49ff27c0
Susan Li <susansemail195@gmail.com>
2026-02-08 12:57:07 -0800

    Scaffold

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import compression from 'compression';
+
+// Security middleware
+import helmet from 'helmet';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: `${process.env.SERVER_URL}:${process.env.SERVER_PORT}`,
+    credentials: true,
+  })
+);
+app.use(helmet());
+app.use(compression());
+
+const server = app.listen(Number(process.env.SERVER_PORT), String(process.env.SERVER_IP), () => {
+  mongoose.set('strictQuery', false);
+  mongoose.connect(`${process.env.MONGO_URL}`).catch(() => {
+    console.error(
+      "Initial Connection Failed - Ensure you've configured your environment's MONGO_URL!"
+    );
+  });
+
+  const db = mongoose.connection;
+  db.on('error', () => {
+    console.error('Could not connect to Mongo - restart the server.');
+  });
+  db.once('open', () => {
+    console.log('Connected to MongoDB');
+  });
+
+  console.log(`Listening at ${process.env.SERVER_URL}:${process.env.SERVER_PORT}`);
+});
+
+server.on('error', console.error);
