f4093a8d44286439fb21ee989900b3e5cb486720
Susan Li <susansemail195@gmail.com>
2026-03-07 17:04:25 -0800

    Complete working image generation and cloud upload

--- src/controllers/HealthController.ts
+++ src/controllers/HealthController.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from 'express';
 import { ResponseCodes } from "../config/enums/ResponseCodes.js";
-import { generateLocationPrompt } from '../models/TravelModel.js';
+import { generateLocationImage, generateLocationPrompt } from '../models/TravelModel.js';
+import { RARE_LOCATION_PROBABILITY } from '../config/constants/contants.js';
 
 async function getHealth(_req: Request, res: Response) {
 
-  console.log(generateLocationPrompt(true));
+  const isRare = Math.random() <= RARE_LOCATION_PROBABILITY;
+
+  const prompt = generateLocationPrompt(isRare);
+
+  const imgUrl = await generateLocationImage(isRare, prompt);
+
+  console.log("Img Url: ", imgUrl);
 
   res.status(ResponseCodes.OK).send({ msg: 'Healthy!' });
 }
--- src/models/TravelModel.ts
+++ src/models/TravelModel.ts
@@ -1,4 +1,5 @@
 import { gemini } from "../config/ai-models/gemini.js";
+import cloudinary from "../config/cloudinary/cloudinary.js";
 import { RARE_LOCATION_PROBABILITY } from "../config/constants/contants.js";
 import {
   commonLocationInstruction,
@@ -25,14 +26,33 @@ export async function generateLocationImage(
   basePrompt: string,
 ) {
   try {
-    const prompt = `${basePrompt} ${isRare ? rareLocationInstruction : commonLocationInstruction}.`;
+    const prompt = `Create an illustration in a kawaii, nostalgic, colorful, and hand-drawn style similar to the art style of the game Tabikaeru with these details: ${basePrompt} ${isRare ? rareLocationInstruction : commonLocationInstruction}.`;
 
     const response = await gemini.models.generateContent({
       model: "gemini-3.1-flash-image-preview",
       contents: prompt,
+      config: {
+        imageConfig: {
+          imageSize: "512px",
+          aspectRatio: "16:9",
+        }
+      }
     });
 
-    
+    const base64Data = response.candidates?.[0]?.content?.parts?.[0]?.inlineData?.data;
+    const mimeType = response.candidates?.[0]?.content?.parts?.[0]?.inlineData?.mimeType || 'image/jpeg';
+
+    if (!base64Data) {
+      throw new Error("No image data returned from the Gemini API.");
+    }
+
+    const dataUri = `data:${mimeType};base64,${base64Data}`;
+
+    const uploadResult = await cloudinary.uploader.upload(dataUri, {
+      folder: "mochita",
+    });
+
+    return uploadResult.secure_url;
 
   } catch (e) {
     console.log("error: ", e);
