62568c02edf4d953c0c5cdad81c9a8f6f06e127e
Susan Li <susansemail195@gmail.com>
2026-03-15 21:02:28 -0700

    Add comments for remaining chat funcs

--- src/models/ChatModel.ts
+++ src/models/ChatModel.ts
@@ -2,17 +2,17 @@ import mongoose from "mongoose";
 import { ChatMemorySchema } from "../config/schemas/ChatMemorySchema.js";
 import { ChatItem } from "../config/interfaces/ChatItem.js";
 import { gemini } from "../config/ai-models/gemini.js";
+import { MAX_MEMORIES_STORED } from "../config/constants/contants.js";
 
-async function retrieveMemories(userId: string) {
+async function retrieveMemories(userId: mongoose.Types.ObjectId) {
   try {
-    const userObjId = new mongoose.Types.ObjectId(userId);
 
-    const memories = await ChatMemorySchema.findById(userObjId).sort({
-      createdAt: -1,
+    const memories = await ChatMemorySchema.find({ userId: userId }).sort({
+      createdAt: -1, // sort by most recent first
     });
 
     if (!memories) {
-      return null;
+      return [];
     }
 
     return memories;
@@ -72,11 +72,46 @@ async function generateNewMemoryFromChat(userId: string, chatHistory: ChatItem[]
     }
     return newMemory;
 
-  } catch (e) {}
+  } catch (e) {
+    console.error("error:", e);
+    return null;
+  }
 }
 
-async function updateMemories() {
-  // this will be called manually (will end the chat) - if not called then memories aren't stored from that chat
+async function updateMemories(userId: string, chatHistory: ChatItem[]) {
+  try {
+
+    // this will be called manually (will end the chat) - if not called then memories aren't stored from that chat
+
+    // check to see how many memories user has
+    const userObjId = new mongoose.Types.ObjectId(userId);
+
+    const memories = await retrieveMemories(userObjId);
+
+    if (memories === null) {
+        return null; // errored out
+    }
+
+    if (memories.length === MAX_MEMORIES_STORED) {
+        // delete oldest one
+        const oldestMemory = memories[memories.length - 1];
+        if (oldestMemory) {
+            const deletedMemory = await ChatMemorySchema.findByIdAndDelete(oldestMemory._id);
+            if (!deletedMemory) {
+                return null;
+            }
+        }
+    }
+    // create new memory from chat
+    const newMemory = await generateNewMemoryFromChat(userId, chatHistory);
+
+    return newMemory;
+
+  } catch (e) {
+    console.error("error:", e);
+    return null;
+  }
+   
 }
 
 async function createUserContext() {
@@ -85,13 +120,22 @@ async function createUserContext() {
   // this will be deleted at the end of a chat (handled in separate function)
 }
 
-async function endChat() {}
-
 async function chatCleanup() {
   // run this when the user enters the chat if they exited without ending chat properly
 }
 
-async function chat() {}
+async function endChat() {
+    // call chat cleanup
+    // update memories
+
+}
+
+async function chat() {
+    // essentially, each turn of the chat will call this func, it will recieve an array of chatitems
+    // chat has a max of 25 turns, e.g. 50 chat items
+    // after which the chat will end and memory will update 
+
+}
 
 export const ChatModel = {
   chat,
