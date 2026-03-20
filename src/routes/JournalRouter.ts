45f2f8ee6bb2c5273708cfcef7697e866b27e338
Susan Li <susansemail195@gmail.com>
2026-03-19 17:16:36 -0700

    Fix issue with extra routes

--- src/routes/JournalRouter.ts
+++ src/routes/JournalRouter.ts
@@ -6,10 +6,5 @@ export const JournalRouter: Router = express.Router();
 JournalRouter.route("/createEntry").post(JournalController.createJournalEntry);
 JournalRouter.route("/updateEntry").put(JournalController.updateJournalEntry);
 JournalRouter.route("/deleteEntry").delete(JournalController.deleteJournalEntry);
-
-JournalRouter.route("/createTag").post(JournalController.createJournalTag);
-JournalRouter.route("/updateTag").put(JournalController.updateJournalTag);
-JournalRouter.route("/deleteTag").delete(JournalController.deleteJournalTag);
-
 JournalRouter.route("/getEntries").post(JournalController.getJournalEntriesByDate);
-JournalRouter.route("/getUserTags").post(JournalController.getUserTags);
+JournalRouter.route("/getTags").post(JournalController.getAllTags);
